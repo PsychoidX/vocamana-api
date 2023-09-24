018ffd5714f224a2db195d7cb52b2de5abc202b9
PsychoidX <psychoidx99@gmail.com>
2023-09-24 23:36:01 +0900

    usersテーブルを作成

---
+++ api/entity/user.go
@@ -0,0 +1,11 @@
+package entity
+
+import "time"
+
+type User struct {
+	Id        uint
+	Email     string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
---
+++ db/goose/migrations/20230924142254_create_users_table.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE SEQUENCE user_id_seq;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  email VARCHAR(400) NOT NULL UNIQUE,
+  password VARCHAR(200) NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER refresh_users_updated_at
+  BEFORE UPDATE ON users FOR EACH ROW
+EXECUTE PROCEDURE refresh_updated_at();
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TRIGGER refresh_users_updated_at ON users;
+DROP TABLE users;
+DROP SEQUENCE user_id_seq;
+-- +goose StatementEnd
