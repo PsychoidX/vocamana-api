071e695a3a5962ac58052f3693ca26997abedb06
PsychoidX <psychoidx99@gmail.com>
2023-10-06 00:17:17 +0900

    main処理作成

--- api/server.go
+++ api/server.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"api/db"
+	"api/repository"
+	"api/usecase"
+	"api/controller"
 )
 
 func main() {
 	e := echo.New()
+	db := db.NewDB()
+	wr := repository.NewWordRepository(db)
+	wu := usecase.NewWordUsecase(wr)
+	wc := controller.NewWordController(wu)
+	e.GET("/words", wc.GetAllWords)
 	e.Logger.Fatal(e.Start(":8080"))
 }
