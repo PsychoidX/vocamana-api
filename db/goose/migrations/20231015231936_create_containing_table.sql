5badb04e50b2bc9202edcee6d94011950671460a
PsychoidX <psychoidx99@gmail.com>
2023-10-17 22:31:46 +0900

    containingテーブルに外部キー参照時の制約を追加

--- db/goose/migrations/20231015231936_create_containing_table.sql
+++ db/goose/migrations/20231015231936_create_containing_table.sql
@@ -4,8 +4,12 @@ CREATE TABLE containing (
   sentence_id INTEGER,
   word_id INTEGER,
   PRIMARY KEY(sentence_id, word_id),
-  FOREIGN KEY (sentence_id) REFERENCES sentences(id) ON DELETE CASCADE,
-  FOREIGN KEY (word_id) REFERENCES words(id) ON DELETE CASCADE
+  FOREIGN KEY (sentence_id) REFERENCES sentences(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (word_id) REFERENCES words(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 -- +goose StatementEnd
 
