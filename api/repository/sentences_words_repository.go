87f385fc1897c5b187da69d1c81b73a44d208b3e
PsychoidX <psychoidx99@gmail.com>
2023-10-19 23:55:20 +0900

    リファクタリング

--- api/repository/sentences_words_repository.go
+++ api/repository/sentences_words_repository.go
@@ -22,11 +22,11 @@ func (swr *SentencesWordsRepository) AssociateSentenceWithWord(sentenceId uint64
 		"INSERT INTO sentences_words" +
 		" (sentence_id, word_id)" +
 		" SELECT $1, $2" +
-		"   WHERE NOT EXISTS(" +
-		"     SELECT 1" +
-		"     FROM sentences_words" +
-		"     WHERE sentence_id = $1 AND word_id = $2" +
-		"   );",
+		" WHERE NOT EXISTS(" +
+		"   SELECT 1" +
+		"   FROM sentences_words" +
+		"   WHERE sentence_id = $1 AND word_id = $2" +
+		" );",
 		sentenceId,
 		wordId,
 	)
