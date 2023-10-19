69378ba478ea09423e903340e1ae0c7904bfcb2b
PsychoidX <psychoidx99@gmail.com>
2023-10-20 00:35:28 +0900

    notationsテーブルにcreated_at, updated_atを追加

--- db/goose/migrations/20231019152512_create_notations.sql
+++ db/goose/migrations/20231019152512_create_notations.sql
@@ -5,11 +5,18 @@ CREATE TABLE notations (
   notation VARCHAR(100),
   FOREIGN KEY (word_id) REFERENCES words(id)
     ON DELETE CASCADE
-    ON UPDATE CASCADE
+    ON UPDATE CASCADE,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TRIGGER refresh_notations_updated_at
+  BEFORE UPDATE ON notations FOR EACH ROW
+EXECUTE PROCEDURE refresh_updated_at();
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP TRIGGER refresh_notations_updated_at ON notations;
 DROP TABLE notations;
 -- +goose StatementEnd
