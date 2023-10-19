ad4910b3b063b60f9cd9ea67f67ce24b9e2a4543
PsychoidX <psychoidx99@gmail.com>
2023-10-20 00:44:35 +0900

    notationsテーブルにidカラムを追加

--- db/goose/migrations/20231019152512_create_notations.sql
+++ db/goose/migrations/20231019152512_create_notations.sql
@@ -1,7 +1,8 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE notations (
-  word_id INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
+  word_id INTEGER,
   notation VARCHAR(100),
   FOREIGN KEY (word_id) REFERENCES words(id)
     ON DELETE CASCADE
