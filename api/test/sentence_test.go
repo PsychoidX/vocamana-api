e0b00117950c2c100f6f05721e3eb835e1acf5e1
PsychoidX <psychoidx99@gmail.com>
2023-10-27 01:38:04 +0900

    Sentenceのテストの雛形作成

---
+++ api/test/sentence_test.go
@@ -0,0 +1,52 @@
+package test
+
+import "testing"
+
+func TestGetAllSentences(t *testing.T) {
+	// ログイン中のUserに紐づくSentenceを取得できることをテスト
+	// TODO ログイン機能
+	// とりあえずuser_id=1のWordのみ取得可能とする
+
+	// TODO
+}
+
+func TestGetSentenceById(t *testing.T) {
+	// ログイン中のUserに紐づくSentenceを取得できることをテスト
+	// TODO ログイン機能
+	// とりあえずuser_id=1のWordのみ取得可能とする
+
+	// TODO
+}
+
+func TestCreateSentence(t *testing.T) {
+	// ログイン中のUserに紐づくSentenceを作成できることをテスト
+	// TODO ログイン機能
+	// とりあえずuser_id=1のWordのみ作成可能とする
+
+	// TODO
+}
+
+func TestUpdateSentence(t *testing.T) {
+	// ログイン中のUserに紐づくSentenceを更新できることをテスト
+	// TODO ログイン機能
+	// とりあえずuser_id=1のWordのみ更新可能とする
+
+	// TODO
+}
+
+func TestDeleteSentence(t *testing.T) {
+	// ログイン中のUserに紐づくSentenceを削除できることをテスト
+	// TODO ログイン機能
+	// とりあえずuser_id=1のWordのみ削除可能とする
+
+	// TODO
+}
+
+func TestAssociateSentenceWithWords(t *testing.T) {
+	// WordとSentenceがどちらもログイン中のUserに紐づく場合
+	// それらを紐づかせられることをテスト
+	// TODO ログイン機能
+	// とりあえずuser_id=1のWordのみ削除可能とする
+
+	// TODO
+}
